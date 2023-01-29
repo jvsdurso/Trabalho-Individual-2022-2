aba84a17ebd8efb0dcaf41799f7a0f096c595933
jvsdurso <69814362+jvsdurso@users.noreply.github.com>
2023-01-29 19:12:00 +0530

    Adicionando testes que o monitor enviou

--- tests/data_pipeline/feature_engineering/__init__.py
+++ tests/data_pipeline/feature_engineering/__init__.py
@@ -5,5 +5,5 @@ import inspect
 borala = inspect.getfile(inspect.currentframe())
 currentdir = os.path.dirname(
     os.path.abspath(borala))
-parentdir = os.path.dirname(os.path.dirname(currentdir))
+parentdir = os.path.dirname(os.path.dirname(os.path.dirname(currentdir)))
 sys.path.insert(0, f'{parentdir}/src')
